4662130482181ad5cb3695902a37c9a08a0d4d99
vladpsel <vpanovwr@gmail.com>
2022-01-29 03:49:05 +0200

    update(head.php): setup asset paths

--- view/user/layout/head.php
+++ view/user/layout/head.php
@@ -10,12 +10,12 @@
     <link type="image/png" sizes="120x120" rel="icon" href="img/favicon.png">
     <meta name="description" content="Влад Панов - версткальщик, Junior php developer">
     <!-- Fonts -->
-    <link rel="stylesheet" href="/css/font.css">
+    <link rel="stylesheet" href="<?php echo $this->getAsset('css/font.css'); ?>">
     <!-- Styles -->
-    <link rel="stylesheet" href="/css/reset.css">
-    <link rel="stylesheet" href="/css/style.css">
-    <link rel="stylesheet" href="/css/media.css">
-    <link rel="stylesheet" href="/css/animation.css">
+    <link rel="stylesheet" href="<?php echo $this->getAsset('css/reset.css'); ?>">
+    <link rel="stylesheet" href="<?php echo $this->getAsset('css/style.css'); ?>">
+    <link rel="stylesheet" href="<?php echo $this->getAsset('css/media.css'); ?>">
+    <link rel="stylesheet" href="<?php echo $this->getAsset('css/animation.css'); ?>">
 </head>
 
 <body>
